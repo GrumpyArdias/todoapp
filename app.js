498fab363ff03bafb89ad211ad087a18dde724ce
M.Herrero <this.mario@outlook.com>
2021-07-21 00:55:28 +0200

    Boton finalizado V1

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ root.appendChild(btn);
 btn.type = "button";
 // Style
 btn.style.boxShadow = "0px 0px 0px 2px #9fb4f2";
-btn.style.backgroundColor = "7892c2";
+btn.style.backgroundColor = "#7892c2";
 btn.style.borderRadius = "10px";
 btn.style.border = "1px solid #4e6096";
 btn.style.display = "inline-block";
@@ -16,9 +16,9 @@ btn.style.fontSize = "19px";
 btn.style.padding = "12px 37px";
 btn.style.textDecoration = "none";
 btn.style.textShadow = btn.innerHTML = "boton de mierda";
+btn.style.btn.classList.add("boton");
 
 // Adding a class
-btn.classList.add("boton");
 
 // 	background:linear-gradient(to bottom, #7892c2 5%, #476e9e 100%); te falta esto
 // 	text-decoration:none;
